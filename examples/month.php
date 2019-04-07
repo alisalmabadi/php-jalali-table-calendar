2e9971a856a066c069c6877db63c3357af13aa42
seyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-04-06 22:19:24 -1200

    example for one month

---
+++ examples/month.php
@@ -0,0 +1,32 @@
+﻿<html>
+<head>
+<title>Persian Calendar For Year 1397 farvadin</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style type="text/css">
+<!--
+Body
+{  
+background-color: #EEEEDD;
+font-family: tahoma; 
+color: #000000;
+font-size: 14 px;
+margin-top: 0;
+margin-left: 0
+}
+</style>
+</head>
+
+<body>
+<?php
+include "../Calendar.Class.php";
+$c = new Calendar;
+$c->SetStyle();
+?>
+<div align="center">
+
+<?php $c->ShowJalaliMonth(1398,1) ?></td>
+
+</div>
+
+</body>
+</html>

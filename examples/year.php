2eddec785eb91ec877be33e43c918bbb4ba8da56
seyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-04-06 22:44:04 -1200

    updated
    some appreance changes!

--- examples/year.php
+++ examples/year.php
@@ -3,11 +3,18 @@
 <title>Persian Calendar For Year 1398</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <style type="text/css">
-<!--
+@font-face {
+  font-family: 'Tanha';
+  src: url('fonts/Tanha-FD.eot') format('eot'),  
+       url('fonts/Tanha-FD.woff') format('woff'),
+       url('fonts/Tanha-FD.ttf') format('truetype');  
+  font-style:normal;
+  font-weight:normal;
+}
 Body
 {  
 background-color: #EEEEDD;
-font-family: tahoma; 
+font-family: Tanha; 
 color: #000000;
 font-size: 14 px;
 margin-top: 0;
@@ -23,7 +30,7 @@ $c = new Calendar;
 $c->SetStyle();
 ?>
 <div align="center">
-<table width="760">
+<table width="85%">
 <tr>
 <td valign="top"><?php $c->ShowJalaliMonth(1398,3) ?></td>
 <td valign="top"><?php $c->ShowJalaliMonth(1398,2) ?></td>

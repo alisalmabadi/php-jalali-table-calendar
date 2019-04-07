5882684bcd413a498bfa707efad8da8293043f5f
seyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-04-06 22:19:45 -1200

    example of showing calendar for 1 year

---
+++ examples/year.php
@@ -0,0 +1,51 @@
+﻿<html>
+<head>
+<title>Persian Calendar For Year 1398</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style type="text/css">
+<!--
+Body
+{  
+background-color: #EEEEDD;
+font-family: tahoma; 
+color: #000000;
+font-size: 14 px;
+margin-top: 0;
+margin-left: 0
+}
+</style>
+</head>
+
+<body>
+<?php
+include "../Calendar.Class.php";
+$c = new Calendar;
+$c->SetStyle();
+?>
+<div align="center">
+<table width="760">
+<tr>
+<td valign="top"><?php $c->ShowJalaliMonth(1398,3) ?></td>
+<td valign="top"><?php $c->ShowJalaliMonth(1398,2) ?></td>
+<td valign="top"><?php $c->ShowJalaliMonth(1398,1) ?></td>
+</tr>
+<tr>
+<td valign="top"><?php $c->ShowJalaliMonth(1398,6) ?></td>
+<td valign="top"><?php $c->ShowJalaliMonth(1398,5) ?></td>
+<td valign="top"><?php $c->ShowJalaliMonth(1398,4) ?></td>
+</tr>
+<tr>
+<td valign="top"><?php $c->ShowJalaliMonth(1398,9) ?></td>
+<td valign="top"><?php $c->ShowJalaliMonth(1398,8) ?></td>
+<td valign="top"><?php $c->ShowJalaliMonth(1398,7) ?></td>
+</tr>
+<tr>
+<td valign="top"><?php $c->ShowJalaliMonth(1398,12) ?></td>
+<td valign="top"><?php $c->ShowJalaliMonth(1398,11) ?></td>
+<td valign="top"><?php $c->ShowJalaliMonth(1398,10) ?></td>
+</tr>
+</table>
+</div>
+
+</body>
+</html>
